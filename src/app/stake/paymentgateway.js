8e029f8eb26f7bb6734310338ae93e08c5c8170a
bisham paracha <bishamparacha789@gmail.com>
2023-08-21 15:49:10 +0400

    changed styling again

--- src/app/stake/paymentgateway.js
+++ src/app/stake/paymentgateway.js
@@ -31,7 +31,7 @@ export default function PaymentGateway(){
     return(
         <Pop> 
             <form onSubmit={handlePayment}>
-            <div className=" flex justify-center items-center gap-4 p-4 text-[#DE2A33]">
+            <div className=" flex justify-center items-center gap-4 p-4 text-[#DE2A33] w-screen">
             <label htmlFor="">Enter Amount (CAD): </label>
             <input type="text" placeholder="(+5% Tax)" autoFocus className="outline-none border-2 p-2 border-[#DE2A33] placeholder-[#DE2A33] bg-black rounded-md w-[20vw] h-[5vh]" onChange={e => setPrice(e.target.value)} onInput={e => {e.target.value = e.target.value.replace(/[^0-9]/g, '')}} />
             </div>
